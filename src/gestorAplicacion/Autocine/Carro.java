bc5a1d57e9741b301b7ff08695c0f308d8a5dd4a
Jega-21 <jhgallegoa@unal.edu.co>
2022-05-23 18:18:50 -0500

    AutoCine
    Clase Carro

--- src/gestorAplicacion/Autocine/Carro.java
+++ src/gestorAplicacion/Autocine/Carro.java
@@ -1,5 +1,22 @@
 package gestorAplicacion.Autocine;
 
+// Clase.
 public class Carro {
+	
+	// Atributos.
+	private String placa;
+	
+	// Constructor.
+	public Carro(String placa) {
+		this.placa = placa;
+	}
 
+	// Getters and Setters.
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	
 }
