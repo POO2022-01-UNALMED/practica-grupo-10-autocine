c39503168917424ffcf4bf2220df4850db894bfb
jiurgi <101530723+jiurgi@users.noreply.github.com>
2022-05-29 09:46:20 -0500

    Update Sala2D.java

--- src/gestorAplicacion/Salas/Sala2D.java
+++ src/gestorAplicacion/Salas/Sala2D.java
@@ -1,4 +1,54 @@
 package gestorAplicacion.Salas;
+import java.io.Serializable;
+import java.util.*;
+import gestorAplicacion.Autocine.*;
 
-public class Sala2D {
+public class Sala2D extends Sala implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private int cantidadGafas;
+	
+	public Sala2D(int filas, int columnas, int filaspreferencial, Autocine cine) {
+		super(filas, columnas, filaspreferencial, 2000, autocine);
+	}
+	
+	public Sala2D(int preferencial, Autocine autocine) {
+		this(8, 12, preferencial, autocine);
+	}
+	
+	public int cantidadPuestos() {
+	/*No recibe nada y devuelve un entero el cual corresponde a la cantidad de puestos
+	disponibles*/
+
+		return super.puestos.size();
+	}
+	
+	public void crearPuestos() {			
+	/*No recibe ningun parametro y no retorna nada
+	Crea cada puesto según la cantidad de filas prefencial, filas, y columnas*/
+		
+	
+		int total = filas*columnas; 			//numero de puestos
+
+		int totalpreferencial = filaspreferencial*columnas;		//numero de puestos prefe y se le resta 1 cada que se compre uno
+														
+
+		String tipo = "PREFENCIAL";					//tipo de puesto se cambia  cuando totalprerencial sea = a 0
+		
+		for(int i = 0;i<total;i++) {			//for que itera la cantidad de puestos 
+			
+			if(totalpreferencial<=0) {					//si se acabaron los preferencial cambia tipo a GENERAL
+				tipo = "GENERAL";
+			}
+			else {								//sino  se reduce totalpreferencial en uno
+				totalpreferencial--;
+			}
+			
+			Puesto puesto = new Puesto(tipo,i+1);	
+												
+
+			puestos.add(puesto);					
+		}
+	}
+
 }
