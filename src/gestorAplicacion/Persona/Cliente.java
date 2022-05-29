7350b970859874ba89a8e27a96a59f9de6abd96d
jiurgi <101530723+jiurgi@users.noreply.github.com>
2022-05-29 08:34:34 -0500

    Update Cliente.java

--- src/gestorAplicacion/Persona/Cliente.java
+++ src/gestorAplicacion/Persona/Cliente.java
@@ -1,16 +1,24 @@
 package gestorAplicacion.Persona;
+import java.io.Serializable;
+import java.util.*;
+import gestorAplicacion.taquilla.*;
+
 
 /**
  * Clase Cliente, lleva todo lo relativo a la información del espectador
  *
  */
-public class Cliente {
+public class Cliente implements Serializable {
  
     /*Atributos*/
     private String nombre;
     private int edad;
     private int id;
     private double dinero;
+    private static final long serialVersionUID = 1L;
+	private String ocupacion;
+	private List<Ticket> historialCompras= new ArrayList<Ticket>(); //una lista con los boletos que ha comprado el cliente en su vida
+	private Autocine autocine;
     
  
     public double getDinero() {
@@ -22,13 +30,33 @@ public class Cliente {
 	}
 
 	/*Constructores*/
-    public Cliente(String nombre, int edad, int cedula) {
+    public Cliente(String nombre, int edad, int cedula, Autocine autocine) {
         this.nombre = nombre;
         this.edad = edad;
         this.id = id;
+        this.autocine=autocine;
     }
+    
+    public String GeneroMasVisto() {
+		/*
+		No recibe nada y devuelve string del género más visto del cliente
+		 */
+		List<String> genreList=new ArrayList<String>();		//lista con los generos que ha visto el cliente
+		for(Ticket ticket: historialCompras) {
+			genreList.add(ticket.getFuncion().getPelicula().getGenero()); 	//Recorre el historial de compras del cliente y anexa de los boletos sus generos
+		}
+		List<Integer> veces=new ArrayList<Integer>();		//lista para guardar la frecuencia de cada genero
+		for(String genre: genreList) {
+			int num = Collections.frequency(genreList, genre);	//De la lista de géneros extrae la frecuencia de cada elemento
+			veces.add(num);
+		}
+		
+		return genreList.get(cuenta.indexOf(Collections.max(cuenta))); 	//se devuelve el genero de la lista que tenga el indice correspondiente al maximo de las ocurrencias de cada genero
+		
+	}
+    
  
-    /*Metodos*/
+    /*set y get*/
     public String getNombre() {
         return nombre;
     }
@@ -56,7 +84,7 @@ public class Cliente {
     /**
      * Pagamos la entrada del cine
      *
-     * @param precio
+     * 
      */
     public void pagarReserva(double precio) {
         dinero -= precio;
@@ -65,12 +93,28 @@ public class Cliente {
     /**
      * Indicamos si el espectador tiene edad para ver la pelicula 
      *
-     * @param edadMinima
-     * @return
+
      */
     public boolean tieneEdad(int edadMinima) {
         return edad >= edadMinima;
     }
+    
+    public List<Ticket> getHistorialCompras() {
+		return historialCompras;
+	}
+	public void setHistorialCompras(List<Ticket> historialCompras) {
+		this.historialCompras = historialCompras;
+	}
+
+
+	public Autocine getAutocine() {
+		return autocine;
+	}
+
+	public void setAutocine(Autocine autocine) {
+		this.autocine = autocine;
+	}
+	
  
     
     @Override
