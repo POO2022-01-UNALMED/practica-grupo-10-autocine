210b017007c9af4b6a88b1069a63d718cb7e3a0f
Jega-21 <jhgallegoa@unal.edu.co>
2022-05-30 14:27:06 -0500

    Autocine
    uiMain

---
+++ src/uiMain/Main.java
@@ -0,0 +1,35 @@
+package uiMain;
+
+import java.util.*;
+import gestorAplicacion.Taquilla.Pelicula;
+import gestorAplicacion.Autocine.Autocine;
+import gestorAplicacion.Persona.Cliente;
+import baseDatos.Serializador;
+import baseDatos.Deserializador;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("BIENVENIDO AL AUTOCINE");
+		int opcion;
+		Autocine autocine = Deserializador.deserializador();
+		do {
+			System.out.println("¿Que desea hacer?\n" + "1. Vender\n" + "2. Administrar\n" + "3. Cerrar");
+			
+			Scanner entrada = new Scanner(System.in);
+			opcion = entrada.nextInt();
+			Serializador.serializar(autocine);
+			switch(opcion) {
+			case 1: Funciones.clienteNuevoOViejo(autocine);
+			break;
+			
+			case 2: System.out.println("Administrar seleccionado\n");
+			Administrar.Ejecucion(autocine);
+			break;
+			}
+		}
+		while (opcion != 3);
+	}
+
+}
