3722c3bb9a15e8f98a371fd36b94c30ea7e8aca8
jiurgi <101530723+jiurgi@users.noreply.github.com>
2022-05-29 21:23:41 -0500

    Update Ticket.java

--- src/gestorAplicacion/Taquilla/Ticket.java
+++ src/gestorAplicacion/Taquilla/Ticket.java
@@ -1,7 +1,9 @@
 package gestorAplicacion.Taquilla;
 import java.io.Serializable;
+
+import gestorAplicacion.Salas.Puesto;
 import gestorAplicacion.Salas.Puesto.Tipo;
-import gestorAplicacion.Salas;
+import gestorAplicacion.Persona.Cliente;
 
 
 public class Ticket implements Serializable{
@@ -11,23 +13,24 @@ public class Ticket implements Serializable{
 	
 	private int num_puesto;
 	private Tipo tipo_puesto; 
-	private float precio_puesto;
+	private float precioTotal;
 	private boolean estado;
 	private Funcion funcion;
+	private float precio_puesto;
 	
 	
-	public void Ticket(Funcion funcion, Puesto puesto) {
+	public Ticket(Funcion funcion, Puesto puesto) {
 		this.funcion = funcion;
 		this.set_puesto(puesto);
 		this.estado = true;
-		this.precio_puesto = this.precio_puesto();
+		this.precioTotal = this.calcularPrecio();
 	}
 	
 	public float calcularPrecio() {		
 		/*No recibe nada y devuelve un float el cual corresponde al calculo del precio del boleto 
 		que depende del precio de la sala y el precio del puesto */
 		
-			float precio_t=funcion.getSala().getPrecio()+precio_silla;	 	// Se suma el precio de la sala y el precio de la silla
+			float precio_t=funcion.getSala().getPrecio()+precio_puesto;	 	// Se suma el precio de la sala y el precio de la silla
 			
 			return precio_t;
 		}
@@ -39,10 +42,10 @@ public class Ticket implements Serializable{
 	}
 	
 	public String estado() {
-	/* Sin parámetros y retorna un String que corresponde al estado de disponibilidad
+	/* Sin parÃ¡metros y retorna un String que corresponde al estado de disponibilidad
 	 del ticket	
 	 */
-		if(estado) {	//Si la está disponible imprime Libre, si no ocupado 
+		if(estado) {	//Si la estÃ¡ disponible imprime Libre, si no ocupado 
 			return "Libre";
 		}
 		return "Ocupado";		
@@ -50,7 +53,7 @@ public class Ticket implements Serializable{
 	
 	
 	public String tipoString() {
-		/*Sin parámetros  y retorna un String el cual indica el tipo de puesto del ticket
+		/*Sin parÃ¡metros  y retorna un String el cual indica el tipo de puesto del ticket
 		 */
 		if(tipo_puesto==Tipo.PREFERENCIAL) {	//El tipo de la puesto es Preferencial retornara P, si no General
 			return "P";
@@ -66,8 +69,15 @@ public class Ticket implements Serializable{
 	public float getPrecio_puesto() {
 		return precio_puesto;
 	}
-
 	
+	public float getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setPrecioTotal(float precioTotal) {
+		this.precioTotal = precioTotal;
+		
+	}
 	public boolean isEstado() {
 		return estado;
 	}
