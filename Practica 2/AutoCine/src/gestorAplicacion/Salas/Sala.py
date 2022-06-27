e747a9be666977c9a946df61fb91451c5be392f6
daniel-giraldo7 <79713709+daniel-giraldo7@users.noreply.github.com>
2022-06-27 14:38:08 -0500

    clases

---
+++ Practica 2/AutoCine/src/gestorAplicacion/Salas/Sala.py
@@ -0,0 +1,175 @@
+﻿
+#*
+# * @author Daniel Alejandro Giraldo Giraldo.
+# * @param clase Sala.
+# * @summary Clase que tiene la imformacion relacionada con las salas.
+# 
+from gestorAplicacion import Autocine.Autocine
+from gestorAplicacion import Taquilla.Funcion
+
+#clase
+
+class Sala:
+
+    #serializador
+    _SERIALVERSIONUID = 1
+    salass = None
+    @staticmethod
+    def _static_initializer():
+        gestorAplicacion.Salas.Sala.salass = []
+
+    _static_initializer()
+
+    #atributos
+
+
+
+    #Constructores
+
+    def __init__(self, filas, columnas, filaspreferencial, precio, autocine):
+        #instance fields found by Java to Python Converter:
+        self.numero = 0
+        self.filas = 0
+        self.columnas = 0
+        self.filasPreferencial = 0
+        self.precio = 0
+        self.autocine = None
+        self.puestos = []
+        self.funciones = []
+
+        self.filas = filas
+        self.columnas = columnas
+        self.filasPreferencial = filaspreferencial
+        self.precio = precio
+        self.autocine = autocine
+
+
+        self.crearPuestos()
+
+        gestorAplicacion.Autocine.Autocine.agregarSala(self)
+
+        self.numero = gestorAplicacion.Autocine.Autocine.getSalas().size()
+
+
+    #Metodos abtractos
+
+
+    def cantidadPuestos(self):
+        pass
+
+    def crearPuestos(self):
+        pass
+
+    #Metodos
+
+    def agregarFuncion(self, funcion):
+        self.funciones.append(funcion)
+
+    def verificarDisponibilidad(self, dia, mes):
+        consulta = "" + str(dia) + str(mes)
+        fechas = []
+        horarios = []
+        disponibles = ["12:00", "14:00", "16:00", "18:00", "20:00", "22:00"]
+        for func in self.funciones:
+            info = "" + func.getDia() + func.getMes()
+            fechas.append(info)
+            info = ""
+
+
+        i = 0
+        while i < len(fechas):
+            if fechas[i] == consulta:
+                horarios.append(self.funciones[i].getHorario())
+            i += 1
+
+        for horario in horarios:
+            disponibles.remove(horario)
+
+        respuesta = ""
+
+        for d in disponibles:
+            respuesta += d + "\n"
+
+        return respuesta == "12:00\n14:00\n16:00\n18:00\n20:00\n22:00\n"
+
+    def verificarDisponibilidad(self, dia, mes, hora):
+
+
+        consulta = str(dia) + "/" + str(mes) + "/" + hora
+
+        fechasfunciones = []
+        for func in self.funciones:
+            info = func.getDia() + "/" + func.getMes() + "/" + func.getHorario()
+            fechasfunciones.append(info)
+
+            info = ""
+
+        for i in fechasfunciones:
+            if i == consulta:
+                return False
+
+        return True
+    def unoDisponible(self, dia, mes):
+
+
+        consulta = "" + str(dia) + str(mes)
+
+        fechas = []
+
+
+        horarios = []
+
+
+        disponibles = ["12:00", "14:00", "16:00", "18:00", "20:00", "22:00"]
+
+        for func in self.funciones:
+            info = "" + func.getDia() + func.getMes()
+            fechas.append(info)
+            info = ""
+
+
+        i = 0
+        while i < len(fechas):
+            if fechas[i] == consulta:
+                horarios.append(self.funciones[i].getHorario())
+            i += 1
+
+        for horario in horarios:
+            disponibles.remove(horario)
+
+        respuesta = ""
+
+        for d in disponibles:
+            respuesta += d + "\n"
+
+        if len(respuesta) >= 5:
+            return True
+        else:
+            return False
+    def verHorarios(self, dia, mes):
+
+        consulta = "" + str(dia) + str(mes)
+        fechas = []
+        horarios = []
+        disponibles = ["12:00", "14:00", "16:00", "18:00", "20:00", "22:00"]
+
+        for func in self.funciones:
+
+            info = "" + func.getDia() + func.getMes()
+            fechas.append(info)
+            info = ""
+
+
+        i = 0
+        while i < len(fechas):
+            if fechas[i] == consulta:
+                horarios.append(self.funciones[i].getHorario())
+            i += 1
+
+
+//====================================================================================================
+//End of the allowed output for the Free Edition of Java to Python Converter.
+
+//To purchase the Premium Edition, visit our website:
+//https://www.tangiblesoftwaresolutions.com/order/order-java-to-python.html
+//====================================================================================================
---
+++ Practica 2/AutoCine/src/gestorAplicacion/Taquilla/Pelicula.py
@@ -0,0 +1,75 @@
+﻿#*
+# * @author Daniel Alejandro Giraldo Giraldo.
+# * @param clase Pelicula.
+# * @summary Clase que tiene la imformacion relacionada con las peliculas.
+# 
+
+from gestorAplicacion import Autocine.Autocine
+from gestorAplicacion import Salas.Sala
+
+
+# clase
+class Pelicula:
+
+    # serializacion
+    _SERIALVERSIONUID = 1
+    peliculass = None
+    @staticmethod
+    def _static_initializer():
+        gestorAplicacion.Taquilla.Pelicula.peliculass = []
+
+    _static_initializer()
+    #artributos
+
+
+    # constructor
+
+    def __init__(self, nombre, genero, duracion, lenguaje, clasificacion, autocine):
+        #instance fields found by Java to Python Converter:
+        self._nombre = None
+        self._genero = None
+        self._duracion = 0
+        self._lenguaje = None
+        self._clasificacion = 0
+        self._autocine = None
+
+        self._nombre = nombre
+        self._genero = genero
+        self._duracion = duracion
+        self._lenguaje = lenguaje
+        self._clasificacion = clasificacion
+        self._autocine = autocine
+        gestorAplicacion.Autocine.Autocine.agregarPelicula(self)
+
+    # gets y sets
+    def getNombre(self):
+        return self._nombre
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getGenero(self):
+        return self._genero
+    def setGenero(self, genero):
+        self._genero = genero
+
+    def getDuracion(self):
+        return self._duracion
+    def setDuracion(self, duracion):
+        self._duracion = duracion
+
+    def getLenguaje(self):
+        return self._lenguaje
+    def setLenguaje(self, lenguaje):
+        self._lenguaje = lenguaje
+
+    def getClasificacion(self):
+        return self._clasificacion
+    def setClasificacion(self, clasificacion):
+        self._clasificacion = clasificacion
+    @staticmethod
+    def getPeliculas():
+        return gestorAplicacion.Taquilla.Pelicula.peliculass
+
+    @staticmethod
+    def setPeliculas(peliculass):
+        Pelicula.peliculass = peliculass
