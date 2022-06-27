75d96c6deca4900179961a383a566c349817da8c
jiurgi <101530723+jiurgi@users.noreply.github.com>
2022-06-27 13:29:35 -0500

    Add files via upload

---
+++ Practica 2/AutoCine/src/uiMain/User/Excepciones/IngresoDatos.py
@@ -0,0 +1,7 @@
+# Clase base de excepciones relacionadas al ingreso de datos.
+
+from uiMain.User.Excepciones.CineExceptions import CineExceptions
+
+class IngresoDatos(CineExceptions):
+    def __init__(self):
+        pass
